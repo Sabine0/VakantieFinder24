1e76fc658c9c716d2724a462b988764cf4b42507
Sabine0 <sabineschreuderx@hotmail.com>
2021-06-05 22:54:25 +0200

    Changed project configurations

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vakantiefinder'),
+      dir: require('path').join(__dirname, './coverage/VakantieFinder24'),
       subdir: '.',
       reporters: [
         { type: 'html' },
