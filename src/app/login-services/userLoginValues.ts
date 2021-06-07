a55e7baf2d0a527f759937df823a5b708d8e59be
Thierry Brouwer <58725453+ThierryBrouwer@users.noreply.github.com>
2021-06-07 16:07:11 +0200

    Login form

---
+++ src/app/login-services/userLoginValues.ts
@@ -0,0 +1,13 @@
+export class UserLoginValues{
+
+  id: string
+  uname: string
+  pwd: string
+
+  constructor(uname: string, pwd: string){
+
+    this.id = '1'
+    this.uname = uname
+    this.pwd = pwd
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserLoginValues } from "../login-services/userLoginValues";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
+  loginUser(loginform: NgForm)
+  {
+    const loginValues = new UserLoginValues(loginform.value.uname, loginform.value.pwd)
+    console.log(loginValues)
   }
-
 }
+
+
