8a05a068627492e88e222d9b0996d53a04636d14
danial <56028352+danialB06@users.noreply.github.com>
2021-06-30 04:02:58 +0200

    property undefined bug fixed

--- frontend/src/app/results/results.component.ts
+++ frontend/src/app/results/results.component.ts
@@ -7,16 +7,16 @@ import {DataService} from "../data.service";
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
-  winner: string = "br";
+  winner: string = "oops something went wrong";
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.dataService.winner.subscribe(winner => {
-      let winnerDiv = <HTMLInputElement>document.getElementById("winner");
-      console.log(winner);
-      winnerDiv.innerHTML = winner;
+      this.winner = winner;
     });
+    let winnerDiv = <HTMLInputElement>document.getElementById("winner");
+    winnerDiv.innerHTML = this.winner;
   }
 
 }
